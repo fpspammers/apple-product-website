1348c3aebac1804b2c48bcc746645d7270e25d53
04rookie <47923475+04rookie@users.noreply.github.com>
2019-03-08 23:52:35 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,41 @@
+<?php
+//connecting to database
+$conn = mysqli_connect('localhost','temp','unlockstackframe','stackframe');
+
+//flag variable to check if all values are set
+$error=false;
+
+//checking if successfully connected to the database
+if(!$conn)
+{
+    die("Unable to connect.");
+}
+
+//checking and setting flag vaiable
+foreach($_POST['items'] as $fields)
+{
+    if(!isset($_POST[$fields]) || (empty($_POST[$fields])))
+    {
+        $error=true;
+    }
+}
+
+//if fields are left empty
+if($error)
+{   
+    die("Some fields were left empty");
+}
+else
+{   //inserting into database
+    $query = "INSERT INTO form VALUES (\"" . $_POST['txtusernamereg'] . "\", \"" . $_POST['txtpasswordreg'] . "\", \"" . $_POST['txtaddress'] . "\", \"" . $_POST['txtfullname']  .  "\");";
+    $result = mysqli_query($conn, $query);
+    if($result)
+    {
+        echo("done!");
+    }
+    else
+    {
+        echo("You are not registered (not able to store in database)");
+    }
+}
+?>
