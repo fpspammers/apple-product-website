90b84fa2cd1015c97d2f778211ba5b1c27b8b6d1
fpspammers <darcvoid4@gmail.com>
2019-03-16 00:13:37 +0530

    sql schema

--- Apple Website Database.sql
+++ Apple Website Database.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `users` 
 (
 	`user_id` int,
-	`name` varchar(255),
+	`username` varchar(255),
 	`city` varchar(255),
 	`password` int,
-	`email` varchar(255)
+	`full_name` varchar(255)
 );
 
 CREATE TABLE `cities` 
@@ -15,28 +15,22 @@ CREATE TABLE `cities`
 	`postal_code` int
 );
 
-CREATE TABLE `products` 
-(
-	`product_id` int,
-	`product_name` varchar(255),
-	`base_shipping` int
-);
-
 CREATE TABLE `product_type` 
 (
 	`type_id` int,
+	`city_id` int,
 	`type_name` varchar(255),
-	`product_id` int
+	`base_shipping` int
 );
 
-CREATE TABLE `variants` 
+CREATE TABLE `specifications` 
 (
-	`variant_id` int,
-	`model_year` int,
+	`spec_id` int,
+	`spec_name` varchar(255),
 	`price` int,
 	`type_id` int
 );
 
-ALTER TABLE `variants` ADD FOREIGN KEY (`type_id`) REFERENCES `product_type` (`type_id`);
+ALTER TABLE `specifications` ADD FOREIGN KEY (`type_id`) REFERENCES `product_type` (`type_id`);
 
-ALTER TABLE `product_type` ADD FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+ALTER TABLE `product_type` ADD FOREIGN KEY (`city_id`) REFERENCES `cities` (`city_id`);
